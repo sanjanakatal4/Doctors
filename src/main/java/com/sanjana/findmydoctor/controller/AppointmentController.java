35b4bcccad6aad492146712238f178c473f6a278
sanjanakatal4 <sanjanakatal4@gmail.com>
2026-05-19 23:32:24 +0530

    Update AppointmentController.java

--- src/main/java/com/sanjana/findmydoctor/controller/AppointmentController.java
+++ src/main/java/com/sanjana/findmydoctor/controller/AppointmentController.java
@@ -27,7 +27,7 @@ import jakarta.servlet.http.HttpSession;
 public class AppointmentController {
 	
 	//String URL="http://localhost:9091/";
-	String URL="http://doctorwebservice-production.up.railway.app/";
+String URL="https://doctorwebservice-production.up.railway.app/";
 	RestTemplate restTemplate=new RestTemplate();
 	
 	@PostMapping("/addAppointment")
